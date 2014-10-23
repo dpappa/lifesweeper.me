b4649cd7c9ed11a2725555e4cabe7d6906fe721a
distortia <nickstalter@gmail.com>
2014-10-23 15:12:41 -0400

    added php and sass stuff

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<?php 
+echo '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>LifeSweeper Technologies</title>
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootswatch/3.1.1/yeti/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+  <body>'; 
+  ?>
