da1e285d696ce75fec628b003d5d9a8bb94d2328
distortia <nickstalter@gmail.com>
2014-11-04 15:46:11 -0500

    about shit

---
+++ about.php
@@ -0,0 +1,104 @@
+<?php include __DIR__.'/includes/header.php'; ?>
+  <div class="splash">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <h1>About us</h1>
+            <p>We don't bite</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h1 class="panel-title">The Crew</h1>
+				</div>
+				<div class="panel-body">
+					
+					  <div class="col-sm-6 col-md-4">
+					    <div class="thumbnail">
+					      <img src="/images/nick.jpg" alt="Nick Stalter">
+					      <div class="caption">
+					        <h3>Nicholas Stalter</h3>
+					        <h4>Ring Leader</h4>
+					        <h5>Degree: CSET</h5>
+					        <p>Originally from Florida, Nick came to Toledo for shortly after graduating high school. Nick likes to play music and dreams of opening his own studio.
+					        When he is not working at LifeSweeper or school, he spends the rest of his time working on his startup, Insourc.</p>
+					        <p><a href="http://www.linkedin.com/pub/nicholas-stalter/93/205/4b5/" class="btn btn-primary" role="button"><i class="fa fa-linkedin"></i></a></p>
+					      </div>
+					    </div>
+					  </div>
+					
+					
+					  <div class="col-sm-6 col-md-4">
+					    <div class="thumbnail">
+					      <img src="http://placekitten.com/g/200/200" alt="Christian Angelo">
+					      <div class="caption">
+					        <h3>Christian Angelo</h3>
+					        <h4>Fret Master</h4>
+					        <h5>Degree: CSET</h5>
+					        <p>Originally from Florida, Nick came to Toledo for shortly after graduating high school. Nick likes to play music and dreams of opening his own studio.
+					        When he is not working at LifeSweeper or school, he spends the rest of his time working on his startup, Insourc.</p>
+					        </p>
+					        <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-linkedin"></i></a></p>
+					      </div>
+					    </div>
+					  </div>
+					
+					
+					  <div class="col-sm-6 col-md-4">
+					    <div class="thumbnail">
+					      <img src="/images/darrell.jpg" alt="Darrell Pappa">
+					      <div class="caption">
+					        <h3>Darrell Pappa</h3>
+					        <h4>Raspberry Gi</h4>
+					        <h5>Degree: CSET</h5>
+					        <p>Originally from Florida, Nick came to Toledo for shortly after graduating high school. Nick likes to play music and dreams of opening his own studio.
+					        When he is not working at LifeSweeper or school, he spends the rest of his time working on his startup, Insourc.</p>
+					        </p>
+					        <p><a href="http://www.linkedin.com/pub/darrell-pappa/9b/a53/144" class="btn btn-primary" role="button"><i class="fa fa-linkedin"></i></a></p>
+					      </div>
+					    </div>
+					  </div>
+
+					  <div class="col-sm-6 col-md-4">
+					    <div class="thumbnail">
+					      <img src="/images/matt.jpg" alt="Matt Rich">
+					      <div class="caption">
+					        <h3>Matthew Rich</h3>
+					        <h4>Desktop Dominator</h4>
+					        <h5>Degree: CSET/EET</h5>
+					        <p>Originally from Florida, Nick came to Toledo for shortly after graduating high school. Nick likes to play music and dreams of opening his own studio.
+					        When he is not working at LifeSweeper or school, he spends the rest of his time working on his startup, Insourc.</p>
+					        </p>
+					        <p><a href="http://www.linkedin.com/in/mattrich22" class="btn btn-primary" role="button"><i class="fa fa-linkedin"></i></a></p>
+					      </div>
+					    </div>
+					  </div>
+					
+					
+					  <div class="col-sm-6 col-md-4">
+					    <div class="thumbnail">
+					      <img src="http://placekitten.com/g/200/200" alt="Mike Stetson">
+					      <div class="caption">
+					        <h3>Michael Stetson</h3>
+					        <h4>Master Tinkerer</h4>
+					        <h5>Degree: CSET</h5>
+					        <p>Originally from Florida, Nick came to Toledo for shortly after graduating high school. Nick likes to play music and dreams of opening his own studio.
+					        When he is not working at LifeSweeper or school, he spends the rest of his time working on his startup, Insourc.</p>
+					        </p>
+					        <p><a href="http://www.linkedin.com/pub/michael-stetson/81/153/386/" class="btn btn-primary" role="button"><i class="fa fa-linkedin"></i></a></p>
+					      </div>
+					    </div>
+					  </div>
+				</div>
+			</div>	
+		</div>
+	</div>
+</div>	
+<?php require __DIR__.'/includes/footer.php'; ?>
