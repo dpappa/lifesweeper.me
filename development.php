398cf231c2c43e2ac0abac9240e8f26cd18bea1c
distortia <nickstalter@gmail.com>
2014-11-18 16:11:40 -0500

    fixed merge conflicts with myself

--- development.php
+++ development.php
@@ -1,5 +1,4 @@
 <?php include __DIR__.'/includes/header.php'; ?>
-<<<<<<< HEAD
       <div class="splash">
       <div class="container">
         <div class="row">
@@ -10,18 +9,6 @@
         </div>
       </div>
     </div>    
-=======
-<div class="splash">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <h1>The Development Process</h1>
-        <p>It's all in the mind, you know?</p>
-      </div>
-    </div>
-  </div>
-</div>    
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -30,21 +17,12 @@
             <h1 class="panel-title">Initial Idea</h1>
           </div>
           <div class="panel-body">
-<<<<<<< HEAD
-            <p>Our story starts with Mike watching TV. The show is called "Inside Combat Rescue." In the episode,
-               American soliders in Afghanistan are on a search and rescue mission. The mission takes them to an active minefield.
-                 Unfortunately, the team is stuck waiting for ground forces to sweep for mines before the helicopter can land.
-                 Time is critical when a soldier is bleeding out.</p>
-            <p> Here we will have references to Mike's first idea conception. 
-            Maybe links to video clips on youtube, and how we all first precieved it. </p>
-=======
           <p>The goal of LifeSweeper is to develop a remote controlled minesweeping vehicle to replace the human beings that do it manually. Our major focus is on portability and ease of use.
                 We noticed that there are no other alternatives to what we are doing and feel that we should take the initiative and develop this for the world.</p>
             <p>Our story starts with Mike watching TV. The show is called "Inside Combat Rescue." In the episode,
                American soliders in Afghanistan are on a search and rescue mission. The mission takes them to an active minefield.
                  Unfortunately, the team is stuck waiting for ground forces to sweep for mines before the helicopter can land.
                  Time is critical when a soldier is bleeding out. This is where we step in.</p>
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
           </div>
         </div>
         <i class="fa fa-arrow-down fa-5x"></i>
@@ -53,18 +31,10 @@
             <h1 class="panel-title">First Thoughts</h1>
           </div>
           <div class="panel-body">
-<<<<<<< HEAD
-          <p>Our first few designs looked like a lunch box with treads. We had tossed around the idea of mounting the metal detector under the body of the car vs right in front of it.</p>  
-          <p>Originally, the intent was to control the car through an Android application. The idea was shelved due to time constraints. Instead we opted for web based control.</p>
-          <p>The marking mechanism was originally designed around the use of spray paint. Using different types of paints, we could achieve different goals, such as glow in the dark and infrared.</p>
-     
-=======
           <p>Our first few designs looked like a lunch box with treads. We had tossed around the idea of mounting the metal detector under the body of the car vs right in front of it. Here is the original 3D mockup without treads.</p>  
           <img class="img-responsive" src="/images/firstcar.jpg" alt="First 3D mockup of car"> 
           <p>Originally, the intent was to control the car through an Android application. The idea was shelved due to time constraints. Instead we opted for web based control.</p>
           <p>The marking mechanism was originally designed around the use of spray paint. Using different types of paints, we could achieve different goals, such as glow in the dark and infrared.</p>
-          
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
         </div>
         </div>
         <i class="fa fa-arrow-down fa-5x"></i>
@@ -73,11 +43,6 @@
             <h1 class="panel-title">Revisting the Idea</h1>
           </div>
           <div class="panel-body">
-<<<<<<< HEAD
-          <p>After some thinking, we decided to revist some of our ideas. Some of the biggest ideas we revisted were the marking mechanism, the metal detector placement, and how to control the vehicle.</p>
-          <p>We took inspiration from real life and developed the boom arm to mimic the action of manually sweeping mines. The boom arm will hold the metal detector in front of the vehicle.</p>
-          <p>The marking mechanism got scrapped for a more mechanized system. Using a motor attached to the boom arm, glow sticks are dropped where the metal detector is positioned.</p>            
-=======
           <p></p>
           <img class="img-responsive" src="/images/newmodel.jpg" alt="final 3D mockup of car">
           <p>We took inspiration from real life and developed the boom arm to mimic the action of manually sweeping mines. The boom arm will hold the metal detector in front of the vehicle.</p>
@@ -99,37 +64,18 @@
             <p>Using the Arduino Yun as an access point, we can connect the Raspberry Pi and all of our devices. The Yun will also host the web page we will be using to control the vehicle with.</p>
             <img class="img-responsive" src="/images/yunandpi.jpg" alt="Arduino Yun and Raspberry Pi">
             <p>The motors will be controlled through a webpage using a REST API, we opted for Javascript. Javascript allows us to use keyboard actions to send commands to the Yun. </p>
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
           </div>
         </div>
         <i class="fa fa-arrow-down fa-5x"></i>
         <div class="panel panel-default">
           <div class="panel-heading">
-<<<<<<< HEAD
-            <h1 class="panel-title">Car Construction</h1>
-          </div>
-          <div class="panel-body">
-            <p>Here we will discuss our current progress. This will include any challenges we faced, and how we aim to overcome them. We can talk about pending plans for the future, and how they might affect our relese date.</p>
-=======
             <h1 class="panel-title">Final moments</h1>
           </div>
           <div class="panel-body">
             <p>Last few things before car is finished and goes on to project page</p>
-            <img class="img-responsive" src="/images/working1.jpg" alt="Working on car">
             <img class="img-responsive" src="/images/working2.jpg" alt="Working on car">
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
           </div>
         </div>
-        <i class="fa fa-arrow-down fa-5x"></i>
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h1 class="panel-title">Final moments</h1>
-          </div>
-          <div class="panel-body">
-            <p>Last few things before car is finished and goes on to project page</p>
-          </div>
-        </div>
-
       </div>
     </div>
   </div>  
--- sponsors.php
+++ sponsors.php
@@ -18,10 +18,9 @@
 					<h1 class="panel-title">Sponsors</h1>
 				</div>
 				<div class="panel-body">
-<<<<<<< HEAD
 				  <div class="col-sm-6 col-md-4">
 				    <div class="thumbnail">
-				      <img src="images/fusechicken.jpeg" alt="[Fuse] Chicken">
+				      <img src="/images/fusechicken.jpeg" alt="[Fuse] Chicken">
 				      <div class="caption">
 				        <h3>[Fuse] Chicken</h3>
 				        <p>Jon Fawcett and his design firm got their start making accessories for iPhones. His expertise in 3D Printing and materials allows us to turn our designs into full functional piece of machinery</p>
@@ -31,7 +30,7 @@
 				  </div>
 				  <div class="col-sm-6 col-md-4">
 				    <div class="thumbnail">
-				      <img src="images/smartshape.png" alt="SmartShape Design">
+				      <img src="/images/smartshape.png" alt="SmartShape Design">
 				      <div class="caption">
 				        <h3>SmartShape Design</h3>
 				        <p>Mike Maczuzak and SmartShape Design are changing the ways we look at fabrication. With extensive knowledge and skill in custom machinery, SmartShape is designing the future.</p>
@@ -49,42 +48,6 @@
 				      </div>
 				    </div>
 				  </div>
-=======
-					
-					  <div class="col-sm-6 col-md-4">
-					    <div class="thumbnail">
-					      <img src="/images/fusechicken.jpeg" alt="[Fuse] Chicken">
-					      <div class="caption">
-					        <h3>[Fuse] Chicken</h3>
-					        <p>Jon Fawcett and his design firm got their start making accessories for iPhones. His expertise in 3D Printing and materials allows us to turn our designs into full functional piece of machinery</p>
-					        <p><a href="http://www.fusechicken.com/" class="btn btn-primary btn-block" role="button">Website</a></p>
-					      </div>
-					    </div>
-					  </div>
-					
-					
-					  <div class="col-sm-6 col-md-4">
-					    <div class="thumbnail">
-					      <img src="/images/smartshape.png" alt="SmartShape Design">
-					      <div class="caption">
-					        <h3>SmartShape Design</h3>
-					        <p>Mike Maczuzak and SmartShape Design are changing the ways we look at fabrication. With extensive knowledge and skill in custom machinery, SmartShape is designing the future.</p>
-					        <p><a href="https://smartshapedesign.com/" class="btn btn-primary btn-block" role="button">Website</a></p>
-					      </div>
-					    </div>
-					  </div>
-					
-					
-					  <div class="col-sm-6 col-md-4">
-					    <div class="thumbnail">
-					      <img src="http://placekitten.com/g/300/300" alt="Ron Rich">
-					      <div class="caption">
-					        <h3>Ron Rich</h3>
-					        <p>Our CTO and Mechanical Engineer, Ron has helped us out tremendously when it came to the mechanics of project. His insight in design and materials helped pave the way to our final product. </p>
-					      </div>
-					    </div>
-					  </div>
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
 				</div>
 			</div>	
 		</div>
@@ -105,13 +68,8 @@
 					      <img src="http://placekitten.com/g/300/300" alt="Jacob Phillips">
 					      <div class="caption">
 					        <h3>Jacob Phillips</h3>
-<<<<<<< HEAD
-					        <p></p>
-					        <p><a href="#" class="btn btn-primary btn-block" role="button">Website</a></p>
-=======
 					        <p>Jacob Phillips, is our go to man for anything Arduino. We are fortunate to have him as an advisor since he teaches the Micro Computer Architecture class, which has us programming with Arduino Uno's</p>
 					        <p><a href="http://www.linkedin.com/pub/jacob-phillips/5b/b98/570" title="Jacob Phillips LinkedIn"  class="btn btn-primary btn-block button-block" role="button"><i class="fa fa-linkedin"></i></a></p>
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
 					      </div>
 					    </div>
 					  </div>
@@ -122,11 +80,7 @@
 					      <img src="/images/springman.jpg" alt="Richard Springman">
 					      <div class="caption">
 					        <h3>Richard Springman</h3>
-<<<<<<< HEAD
-					        <p></p>
-=======
 					        <p>Richard Spring is more than our Capstone Professor, he is a vital resource that allows us to bounce ideas off of his Mechanical Engineering mind. It helps when none of us are Mechanical Engieers</p>
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
 					        <p><a href="#" class="btn btn-primary btn-block" role="button">Website</a></p>
 					      </div>
 					    </div>
@@ -138,16 +92,11 @@
 					      <img src="/images/evans.jpg" alt="Ted Evans">
 					      <div class="caption">
 					        <h3>Ted Evans</h3>
-<<<<<<< HEAD
-					        <p></p>
-=======
 					        <p>Willam Ted Evans is the Director of EET and a professor of ours. Dr. Evans has extensive knowledge of electronics and points us in the direction of simplifying and constructing circuits.</p>
->>>>>>> bb9a381f0b43ed5a0abf64149109e2e2f0c565a4
 					        <p><a href="#" class="btn btn-primary btn-block" role="button">Website</a></p>
 					      </div>
 					    </div>
 					  </div>
-
 				</div>
 			</div>	
 		</div>
@@ -161,7 +110,6 @@
 					<h1 class="panel-title">Special Thanks</h1>
 				</div>
 				<div class="panel-body">
-
 				<p>On top of our wonderful sponsors and advisors, there are countless others who helped us get to thwere we are today.</p>
 				</div>
 			</div>	
